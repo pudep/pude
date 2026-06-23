1fec255a36b8ebe8e57f2777a456fd5eb4a08e80
anoninus <anoninuxt@gmail.com>
2026-06-23 20:22:32 +0530

    Slowly improving!

--- src/app/state.rs
+++ src/app/state.rs
@@ -1,14 +1,19 @@
+use std::io::Read;
+
 use crossterm::cursor;
 use ropey::Rope;
 
 #[derive(Debug)]
 pub struct Buffer {
+  /// The main data
   bufdata: Rope,
+  /// the virtual cursor
+  /// differs from Rope's (col, row)
   cursor: (u16, u16),
 }
 
 impl Default for Buffer {
-  fn default() -> Self{
+  fn default() -> Self {
     Buffer {
       bufdata: Rope::new(),
       cursor: (0, 0),
@@ -17,18 +22,48 @@ impl Default for Buffer {
 }
 
 impl Buffer {
+  /// Initialize new Rope.
+  /// Call once per file only!
   pub fn new() -> Self {
     Self::default()
   }
 
-  pub fn reader(&mut self, str: &str) -> &mut Self {
-    self.bufdata = Rope::from_str(str);
+  /// Read calls Rope::from_reader() 
+  /// Call once per file only!
+  /// Feed a io::BufReader for preformance
+  pub fn read<R: Read>(&mut self, mut reader: R) -> Result<&mut Buffer, std::io::Error> {
+    self.bufdata = Rope::from_reader(&mut reader)?;
+    Ok(self)
+  }
+
+  /// gives (col, row) of virtual/crossterm's 
+  /// terminal cursor
+  pub fn get_virtual_cursor_pos(&mut self) -> &mut Self {
+    let (col, row) = cursor::position().unwrap();
+    self.cursor = (col, row);
     self
   }
 
-  pub fn get_cursor_pos(&mut self) -> &mut Self {
-    let (col, row) = cursor::position().unwrap();
-    self.cursor = (col, row);
+  fn clamp_col(&mut self){
+    
+  }
+
+  /// Takes &mut Buffer and digit: u16
+  /// Could be buggy!
+  pub fn cursor_move_left(&mut self, digit: u16) -> &mut Self {
+    if self.cursor.0 > 0 {
+      self.cursor.0 -= digit;
+    }
+    self
+  }
+
+  /// Takes &mut Buffer and digit: u16
+  /// moves cursor x digits right
+  /// Could be buggy!
+  pub fn move_cursor_left(&mut self, digit: u16) -> &mut Self {
+    if self.cursor.0 != 0 || (self.cursor.0 - digit) > 0 {
+      self.cursor.0 -= digit;
+    }
     self
   }
 }
