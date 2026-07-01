228b22baac17e63212850443715abcd188592c8b
anoninus <anoninuxt@gmail.com>
2026-07-01 11:08:02 +0530

    (programming is fun) Imlementing wrap logic and encountering bugs!

--- src/bin/my_display.rs
+++ src/bin/my_display.rs
@@ -1,6 +1,9 @@
 use std::{error::Error, result};
 
-use crossterm::event::{Event, KeyCode, KeyModifiers};
+use crossterm::{
+  event::{Event, KeyCode, KeyModifiers},
+  style::SetAttribute,
+};
 use pude::state;
 use ratatui::{DefaultTerminal, Frame, widgets::Paragraph};
 use ropey::Rope;
@@ -61,23 +64,56 @@ fn app(
   }
 }
 
+fn push_space_char(width: &mut usize) -> String {
+  let mut string = String::new();
+  while *width != 0 {
+    string.push(' ');
+    *width -= 1;
+  }
+  string
+}
+
+fn smart_soft_wrap(width: u16, text: &str) -> Vec<&str> {
+  let options =
+    textwrap::Options::new(width.into()).word_splitter(textwrap::WordSplitter::NoHyphenation);
+
+  let wrapped_text: Vec<&str> = textwrap::wrap(text, options)
+    .into_iter()
+    .map(|cow| match cow {
+      std::borrow::Cow::Borrowed(s) => s,
+      std::borrow::Cow::Owned(_) => unreachable!("The text was unreachable"),
+    })
+    .collect();
+
+  wrapped_text
+}
+
 fn render(frame: &mut Frame, rope: &Rope, viewport: &mut ViewPort) {
   let terminal_area = frame.area();
   let frame_height = terminal_area.height;
   let frame_width = terminal_area.width;
 
-  let line_number_width = rope.lines().len().to_string().len().max(5);
-  let gutter_padding = " ";
-  let next_number_padding = " ";
+  let mut last_line_width = rope.lines().len().to_string().len().max(5);
+  let number_width = push_space_char(&mut last_line_width);
+  // let gutter_padding = Vec::new();
+  let space = " ";
 
   let mut content = String::new();
   for (idx, line) in rope.lines().skip(viewport.scroll_offset).enumerate() {
     let line = line.to_string();
-
-    // idx == 0; scrolloffset say = 10 and current line is 11 therefore 0 + 10 + extra 1 == 11!
+    let wrapped_text = smart_soft_wrap(frame_width, &line);
+    // idx == 0; scrolloffset say = 10 and current lie is 11 therefore 0 + 10 + extra 1 == 11!
     let line_number = idx + viewport.scroll_offset + 1;
-    let display_line = format!("{gutter_padding}{line_number}{next_number_padding}{line}");
-    content.push_str(&display_line);
+
+    for (idx, line) in wrapped_text.iter().enumerate() {
+      if idx == 0 {
+        let wrap = format!("{space}{space}{line_number}{space}{line}");
+        content.push_str(&wrap);
+      } else if idx != 0 {
+        let wrap = format!("{space}{space}{number_width}{space}{line}");
+        content.push_str(&wrap);
+      }
+    }
   }
 
   frame.render_widget(Paragraph::new(content), terminal_area);
