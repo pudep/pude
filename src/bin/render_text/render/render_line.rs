83e4a0f4daabcc921c6729f24c12caf79ca1bf62
pudep <261843105+pudep@users.noreply.github.com>
2026-07-02 14:36:25 +0530

    getting close in very wrong way

--- src/bin/render_text/render/render_line.rs
+++ src/bin/render_text/render/render_line.rs
@@ -2,27 +2,17 @@ use crate::app_state::*;
 use crate::prelude::normal::*;
 use crate::wrap::util::*;
 pub fn render(frame: &mut Frame, rope: &Rope, viewport: &mut ViewPort) {
-  let terminal_area = frame.area();
-  let mut last_line_width = rope.lines().len().to_string().len().max(5);
-  let number_width = push_space_char(&mut last_line_width);
-  let space = " ";
-
   let mut content = String::new();
+  let terminal_area = frame.area();
+  let mut llnw = rope.lines().len().to_string().len().max(5);
+  let nw = push_space_char(llnw as u16);
+
   for (idx, line) in rope.lines().skip(viewport.scroll_offset).enumerate() {
     let line = line.to_string();
     let wrapped_text = smart_soft_wrap(terminal_area.width, &line);
-    // idx == 0; scrolloffset say = 10 and current lie is 11 therefore 0 + 10 + extra 1 == 11!
     let line_number = idx + viewport.scroll_offset + 1;
-
-    // for (idx, wrap_line) in wrapped_text.iter().enumerate() {
-    //   if idx == 0 {
-        let wrap = format!("{space}{space}{line_number}{space}{wrapped_text}");
-        content.push_str(&wrap);
-  //     } else if idx != 0 {
-  //       let wrap = format!("{space}{space}{number_width}{wrap_line}");
-  //       content.push_str(&wrap);
-  //     }
-  //   }
+    let wrapped_line = smart_soft_wrap(terminal_area.width, &line);
+    content.push_str(&wrapped_line);
   }
   frame.render_widget(Paragraph::new(content), terminal_area);
 }
--- src/bin/render_text/wrap/util.rs
+++ src/bin/render_text/wrap/util.rs
@@ -1,8 +1,9 @@
-pub fn push_space_char(width: &mut usize) -> String {
+pub fn push_space_char(width: u16) -> String {
+  let mut x = width;
   let mut string = String::new();
-  while *width != 0 {
+  while x != 0 {
     string.push(' ');
-    *width -= 1;
+    x -= 1;
   }
   string
 }
@@ -11,6 +12,24 @@ pub fn smart_soft_wrap(width: u16, text: &str) -> String {
   let options =
     textwrap::Options::new(width.into()).word_splitter(textwrap::WordSplitter::NoHyphenation);
 
-  let wrapped_text = textwrap::wrap(text, options).join("\n");
-  wrapped_text
+  let wrapped_text: Vec<&str> = textwrap::wrap(text, options)
+    .into_iter()
+    .map(|cow| match cow {
+      std::borrow::Cow::Borrowed(s) => s,
+      std::borrow::Cow::Owned(_) => unreachable!("The text was unreachable"),
+    })
+    .collect();
+
+  let mut content_line = String::new();
+  for (idx, line) in wrapped_text.into_iter().enumerate() {
+    if idx == 0 {
+      let first_row = format!("  1. {line}\n");
+      content_line.push_str(&first_row);
+    } else if idx > 0 {
+      let wrapped_row = format!("     {line}\n");
+      content_line.push_str(&wrapped_row);
+    }
+  }
+
+  content_line
 }
